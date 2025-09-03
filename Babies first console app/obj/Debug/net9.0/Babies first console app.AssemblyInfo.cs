d816a8e5eb0f4c64e8c65d8915e010fe1889cbf5
Gavin Johnson <Johnsongavin524@gmail.com>
2025-09-03 01:08:48 -0500

    WD week 2 assignment/organization

--- Babies first console app/obj/Debug/net9.0/Babies first console app.AssemblyInfo.cs
+++ Babies first console app/obj/Debug/net9.0/Babies first console app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Babies first console app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f137f4d0eafe36780f147ce9ac23d10052a5b9b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de5bc515526a8780b56f8234689836f30962298b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Babies first console app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Babies first console app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
